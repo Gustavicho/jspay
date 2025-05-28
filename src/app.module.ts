f1ca78b54babbe26fc1a9b97287262d714bec211
Gustavo Oliveira <gustavoooliveiradecarvalho@gmail.com>
2025-05-27 23:54:10 -0400

    feat: configure db conn

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,27 @@ import { WalletModule } from './modules/wallet/wallet.module';
 import { TransaaactionModule } from './modules/transaction/transaction.module';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [WalletModule, TransaaactionModule, UserModule, AuthModule],
+  imports: [WalletModule, TransaaactionModule, UserModule, AuthModule,
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigModule],
+      useFactory: (cs: ConfigService) => ({
+        type: 'postgres',
+        host: cs.get('DATABASE_HOST'),
+        port: +cs.get('DATABASE_PORT'),
+        username: cs.get('DATABASE_USER'),
+        password: cs.get('DATABASE_PASSWORD'),
+        database: cs.get('DATABASE_NAME'),
+        autoLoadEntities: true,
+        synchronize: cs.get('ENV') === 'dev',
+      }),
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
