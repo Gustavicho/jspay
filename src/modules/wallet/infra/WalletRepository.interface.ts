3e99f47c73ddbd186bcd1d5e9f586697d2986a37
Gustavo Oliveira <gustavoooliveiradecarvalho@gmail.com>
2025-06-01 21:28:39 -0400

    fix: remove deprecated files

--- src/modules/wallet/infra/WalletRepository.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Wallet } from "../domain/Wallet";
-
-export default interface IWalletRepository {
-    getWalletByUserId(userId: string): Promise<Wallet>
-    getWalletById(id: string): Promise<Wallet>
-    save(wallet: Wallet): Promise<void>
-    update(wallet: Wallet): Promise<void>
-    delete(wallet: Wallet): Promise<void>
-}
